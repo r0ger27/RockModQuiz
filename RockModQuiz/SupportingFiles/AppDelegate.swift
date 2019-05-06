8baddf1c368c38802069dca7a1373cb7c20df776
r0ger27 <rsafin90@gmail.com>
2019-05-06 15:17:18 +0300

    Initial Commit

---
+++ RockModQuiz/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  RockModQuiz
+//
+//  Created by Ruslan Safin on 06/05/2019.
+//  Copyright © 2019 Ruslan Safin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
---
+++ RockModQuiz/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RockModQuiz
+//
+//  Created by Ruslan Safin on 06/05/2019.
+//  Copyright © 2019 Ruslan Safin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
